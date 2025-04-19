5b280d64c4bb9da60041cde88501aa8e2de459e1
XDdevv <undefineduser087@gmail.com>
2025-04-19 05:53:00 +0500

    Checking/unchecking feature implemented

--- app/src/main/java/zed/rainxch/april2minichallenge/MainActivity.kt
+++ app/src/main/java/zed/rainxch/april2minichallenge/MainActivity.kt
@@ -8,12 +8,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Scaffold
@@ -25,19 +32,34 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import zed.rainxch.april2minichallenge.ui.theme.April2MiniChallengeTheme
 import zed.rainxch.april2minichallenge.ui.theme.BlueDark
+import zed.rainxch.april2minichallenge.ui.theme.Brown
 import zed.rainxch.april2minichallenge.ui.theme.Gray
 import zed.rainxch.april2minichallenge.ui.theme.Orange1
 import zed.rainxch.april2minichallenge.ui.theme.Orange2
 import zed.rainxch.april2minichallenge.ui.theme.Orange3
 import zed.rainxch.april2minichallenge.ui.theme.Yellow
+import zed.rainxch.april2minichallenge.ui.theme.Yellow1
+import zed.rainxch.april2minichallenge.ui.theme.Yellow2
+import zed.rainxch.april2minichallenge.ui.theme.Yellow3
+import zed.rainxch.april2minichallenge.ui.theme.Yellow4
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,12 +68,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             April2MiniChallengeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    EggHuntScreen()
                 }
             }
         }
     }
 }
+
+var nunitoBlack = FontFamily(Font(R.font.nunito_black))
+var nunitoBold = FontFamily(Font(R.font.nunito_semi_bold))
 
 @Preview(showBackground = true)
 @Composable
@@ -62,14 +87,18 @@ fun EggHuntScreen(modifier: Modifier = Modifier) {
             .background(BlueDark),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        var totalEggs by remember { mutableStateOf(0) }
-        var selectedEggs by remember { mutableStateOf(0) }
+        val textGradient = Brush.horizontalGradient(
+            colors = listOf(
+                Color(0xffFFD343),
+                Color(0xffFFD64D),
+                Color(0xffFFB619),
+            )
+        )
 
         var eggs by remember {
             mutableStateOf(
-                listOf<EggItem>(
-                    EggItem("Behind the TV stand", true),
+                mutableListOf<EggItem>(
+                    EggItem("Behind the TV stand"),
                     EggItem("In the garden shed"),
                     EggItem("In the garden shed"),
                     EggItem("Inside the birdhouse"),
@@ -81,43 +110,58 @@ fun EggHuntScreen(modifier: Modifier = Modifier) {
             )
         }
 
+        val totalEggs = eggs.size
+        var selectedEggs by remember { mutableStateOf(eggs.filter { it.isChecked }.size) }
 
         Text(
-            text = stringResource(R.string.egg_hunt_checklist),
+            text = "Egg Hunt Checklist",
             style = TextStyle(
-                Brush.verticalGradient(
-                    colors = listOf(Orange1, Orange2, Orange3)
-                )
-            )
+                brush = textGradient,
+                fontSize = 22.sp,
+                fontFamily = nunitoBlack
+            ),
+            modifier = Modifier.padding(top = 20.dp)
         )
-
+        Spacer(Modifier.height(36.dp))
         Text(
-            text = stringResource(R.string.pick_locations_where_you_ve_found_eggs),
+            text = "Pick locations, where you’ve found eggs",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(horizontal = 32.dp),
+            fontSize = 18.sp,
+            fontFamily = nunitoBlack,
             color = Color.White
         )
-
         Text(
-            text = stringResource(R.string.d_d_eggs_found, selectedEggs, totalEggs),
-            color = Yellow
+            text = "${selectedEggs}/${totalEggs} eggs found",
+            color = Color(0xffFFF583),
+            fontSize = 16.sp,
+            fontFamily = nunitoBold
         )
+
+        Button(
+            onClick = {
+                eggs.clear()
+            }
+        ) {
+            Text("Hallo")
+        }
 
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier.padding(16.dp)
         ) {
-            items(eggs) { egg ->
+            itemsIndexed(eggs) { index, egg ->
                 EggItem(
                     egg = egg,
-                    onCheckedChange = { isChecked, currentEgg ->
-                        if (isChecked && !currentEgg.isChecked) {
+                    onCheckedChange = { isChecked, newEgg ->
+                        if (isChecked) {
                             selectedEggs++
-                            currentEgg.isChecked = true
                         } else {
                             if (selectedEggs > 0) {
                                 selectedEggs--
-                                currentEgg.isChecked = true
                             }
                         }
+                        eggs[index] = newEgg
                     }
                 )
             }
@@ -132,35 +176,45 @@ fun EggItem(
     egg: EggItem,
     onCheckedChange: (Boolean, EggItem) -> Unit
 ) {
-    var eggState by remember { mutableStateOf(egg) }
+    var eggIsCheckedState by remember { mutableStateOf(egg.isChecked) }
+    val bgColor by remember { mutableStateOf(
+        if(eggIsCheckedState) Brush.verticalGradient(listOf(
+            Yellow1,
+            Yellow2,
+            Yellow3,
+            Yellow4
+        )) else SolidColor(Gray)
+    ) }
 
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .background(Gray, RoundedCornerShape(10.dp))
-            .padding(16.dp)
-            .padding(end = 16.dp),
+            .background(bgColor, RoundedCornerShape(16.dp))
+            .padding(12.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Checkbox(
-            checked = eggState.isChecked,
+            checked = eggIsCheckedState,
             onCheckedChange = { isChecked ->
-                eggState.isChecked = isChecked
-                onCheckedChange(isChecked, eggState)
+                eggIsCheckedState = isChecked
+                onCheckedChange(isChecked, egg.copy(isChecked = isChecked))
             },
             colors = CheckboxDefaults.colors(
-                uncheckedColor = Color.Red,
-                checkmarkColor = Color.Black
+                checkedColor = Color.White,
+                uncheckedColor = Color.White,
+                checkmarkColor = Brown
             ),
             modifier = Modifier
-                .padding(end = 16.dp)
-                .background(Color.White)
+                .size(32.dp)
+                .shadow(4.dp, RoundedCornerShape(12.dp))
+                .background(Color.White, RoundedCornerShape(12.dp))
+                .clipToBounds()
         )
-
+        Spacer(Modifier.width(8.dp))
         Text(
             text = egg.locationDescription,
-            color = Color.White
+            color = Color.White,
+            modifier = Modifier.padding(4.dp)
         )
     }
 }
-
--- app/src/main/java/zed/rainxch/april2minichallenge/ui/theme/Color.kt
+++ app/src/main/java/zed/rainxch/april2minichallenge/ui/theme/Color.kt
@@ -7,6 +7,11 @@ val Orange1 = Color(0xFFFFD343)
 val Orange2 = Color(0xFFFFD64D)
 val Orange3 = Color(0xFFFFB619)
 
+val Yellow1 = Color(0xFFFFC441)
+val Yellow2 = Color(0xFFEDA616)
+val Yellow3 = Color(0xFFE09723)
+val Yellow4 = Color(0xFFCA7500)
+
 val Gray = Color (0xFF8D8EA1)
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -17,3 +22,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 val BlueDark = Color(0xFF0F1241)
+
+val Brown = Color(0xff682D0D)
